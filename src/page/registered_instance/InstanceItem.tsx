5321d5d8d938e32333c627a3779b69cf100d92ef
kitakkun <48154936+kitakkun@users.noreply.github.com>
2023-11-28 15:04:14 +0900

    Accordionを使って書き直す

--- src/page/registered_instance/InstanceItem.tsx
+++ src/page/registered_instance/InstanceItem.tsx
@@ -1,7 +1,13 @@
-import {Collapse, List, ListItemButton, ListItemText} from "@mui/material";
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  List,
+  ListItemText
+} from "@mui/material";
 import React from "react";
 import {theme} from "flipper-plugin";
-import {ExpandLess, ExpandMore} from "@mui/icons-material";
+import {ExpandMore} from "@mui/icons-material";
 
 import {NotifyValueChange} from "../../events/FlipperIncomingEvents";
 import {DebuggableStateHolderInfo} from "../../data/RegisterInstance";
@@ -16,9 +22,6 @@ type InstanceItemProps = {
 export default function InstanceItem({instance, onSelectProperty, valueChangedEvents}: InstanceItemProps) {
   const [showProperties, setShowProperties] = React.useState(true);
 
-  const toggleShowProperties = () => {
-    setShowProperties(!showProperties);
-  }
   const getNumOfEvents = (propertyName: string) => valueChangedEvents.filter((event) => event.propertyName == propertyName).length;
 
   const handleOnSelectProperty = (propertyName: string) => {
@@ -26,22 +29,22 @@ export default function InstanceItem({instance, onSelectProperty, valueChangedEv
   }
 
   return (
-    <>
-      <ListItemButton
-        onClick={() => {
-          toggleShowProperties();
-        }}
-        disabled={instance.properties.length == 0}
-      >
+    <Accordion
+      disableGutters={true}
+      expanded={showProperties}
+      disabled={instance.properties.length == 0}
+      sx={{border: "none"}}
+      onChange={(_, expanded) => setShowProperties(expanded)}
+    >
+      <AccordionSummary expandIcon={<ExpandMore/>}>
         <ListItemText
           primary={instance.instanceType}
           primaryTypographyProps={{style: {fontSize: theme.fontSize.default}}}
           secondary={instance.instanceUUID}
           secondaryTypographyProps={{style: {fontSize: theme.fontSize.small}}}
         />
-        {instance.properties.length > 0 ? showProperties ? <ExpandLess/> : <ExpandMore/> : null}
-      </ListItemButton>
-      <Collapse in={showProperties} timeout="auto" unmountOnExit>
+      </AccordionSummary>
+      <AccordionDetails>
         <List component="div" disablePadding>
           {instance.properties.map((property) => (
             <PropertyItem
@@ -51,8 +54,7 @@ export default function InstanceItem({instance, onSelectProperty, valueChangedEv
             />
           ))}
         </List>
-      </Collapse>
-    </>
-  )
-    ;
+      </AccordionDetails>
+    </Accordion>
+  );
 }
