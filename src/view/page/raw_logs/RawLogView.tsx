9189941f652a981251f8da6a4e97bc8cf4ed1933
kitakkun <48154936+kitakkun@users.noreply.github.com>
2024-01-04 20:37:54 +0900

    RawLogViewを標準のDataTableを使って表示する

--- src/view/page/raw_logs/RawLogView.tsx
+++ src/view/page/raw_logs/RawLogView.tsx
@@ -1,37 +1,35 @@
 import React from "react";
 import {RawEventLog} from "../../../data/RawEventLog";
-import {Table} from "antd";
-import {Layout, theme} from "flipper-plugin";
+import {createDataSource, DataSource, DataTable, DataTableColumn, Layout, styled, theme} from "flipper-plugin";
 
 type RawLogPageProps = {
   rawEventLog: RawEventLog[];
 }
 
 export function RawLogView({rawEventLog}: RawLogPageProps) {
-  const dataSource = rawEventLog.map((log, index) => {
+  const dataSource = createDataSource(rawEventLog.map((log) => {
     return {
-      key: index,
       label: log.label,
       payload: JSON.stringify(log.payload),
     }
-  });
+  }));
 
-  const columns = [
+  const columns: DataTableColumn[] = [
     {
       title: 'label',
-      dataIndex: 'label',
       key: 'label',
+      width: 100,
     },
     {
       title: 'payload',
-      dataIndex: 'payload',
       key: 'payload',
+      width: "auto",
     },
   ];
 
   return (
-    <Layout.Container padv={theme.inlinePaddingV} padh={theme.inlinePaddingH} gap={theme.space.medium}>
-      <Table dataSource={dataSource} columns={columns} scroll={{x: true}}/>
+    <Layout.Container padv={theme.inlinePaddingV} padh={theme.inlinePaddingH} gap={theme.space.medium} grow={true}>
+      <DataTable columns={columns} dataSource={dataSource}/>
     </Layout.Container>
   );
 }
