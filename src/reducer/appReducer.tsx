107e5b5057c7f3e801fc12824d4c2c537a78160c
kitakkun <48154936+kitakkun@users.noreply.github.com>
2024-01-05 03:46:19 +0900

    appReducerで不要なものを削除

--- src/reducer/appReducer.tsx
+++ src/reducer/appReducer.tsx
@@ -1,34 +1,18 @@
-import {NotifyValueChange} from "../events/FlipperIncomingEvents";
 import {createSlice} from "@reduxjs/toolkit";
 
 export interface AppState {
   activeTabIndex: string;
-  selectedInstanceUUID: string | null;
-  selectedPropertyName: string | null;
-  selectedPropertyValueChangeLog: NotifyValueChange[];
-  hideNonDebuggableProperties: boolean;
 }
 
 const appSlice = createSlice({
   name: "app",
   initialState: {
     activeTabIndex: '1',
-    selectedInstanceUUID: null,
-    selectedPropertyName: null,
-    selectedPropertyValueChangeLog: [],
-    hideNonDebuggableProperties: true,
   } as AppState,
   reducers: {
     updateActiveTabIndex: (state, action) => {
       state.activeTabIndex = action.payload;
     },
-    selectProperty: (state, action) => {
-      state.selectedInstanceUUID = action.payload.instanceUUID;
-      state.selectedPropertyName = action.payload.propertyName;
-    },
-    hideNonDebuggablePropertiesToggled: (state, action) => {
-      state.hideNonDebuggableProperties = action.payload;
-    }
   }
 });
 
@@ -36,6 +20,3 @@ export const appActions = appSlice.actions;
 export const appReducer = appSlice.reducer;
 
 export const selectActiveTabIndex = (state: any) => state.app.activeTabIndex as string;
-export const selectSelectedInstanceUUID = (state: any) => state.app.selectedInstanceUUID as string | null;
-export const selectSelectedPropertyName = (state: any) => state.app.selectedPropertyName as string | null;
-export const selectHideNonDebuggableProperties = (state: any) => state.app.hideNonDebuggableProperties as boolean;
